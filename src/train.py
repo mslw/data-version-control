3f8c3547c3385f0e0a33aaead95fbc4732966198
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2023-08-07 17:23:49 +0200

    Train Random Forest classifier

--- src/train.py
+++ src/train.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 from skimage.io import imread_collection
 from skimage.transform import resize
-from sklearn.linear_model import SGDClassifier
+
+from sklearn.ensemble import RandomForestClassifier
 
 
 def load_images(data_frame, column_name):
@@ -13,17 +14,14 @@ def load_images(data_frame, column_name):
     image_list = imread_collection(filelist)
     return image_list
 
-
 def load_labels(data_frame, column_name):
     label_list = data_frame[column_name].to_list()
     return label_list
-
 
 def preprocess(image):
     resized = resize(image, (100, 100, 3))
     reshaped = resized.reshape((1, 30000))
     return reshaped
-
 
 def load_data(data_path):
     df = pd.read_csv(data_path)
@@ -33,14 +31,15 @@ def load_data(data_path):
     data = np.concatenate(processed_images, axis=0)
     return data, labels
 
-
 def main(repo_path):
     train_csv_path = repo_path / "data/prepared/train.csv"
     train_data, labels = load_data(train_csv_path)
-    sgd = SGDClassifier(max_iter=10)
-    trained_model = sgd.fit(train_data, labels)
-    dump(trained_model, repo_path / "model/model.joblib")
 
+    rf = RandomForestClassifier()
+
+    trained_model = rf.fit(train_data, labels)
+
+    dump(trained_model, repo_path / "model/model.joblib")
 
 if __name__ == "__main__":
     repo_path = Path(__file__).parent.parent
